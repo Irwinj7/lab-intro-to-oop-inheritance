84ecb775b4548318e47372a990b9461ea8beb562
“irwinj7” <Irwinjorge@pursuit.com>
2023-10-12 03:15:18 -0400

    game is finished

--- food.js
+++ food.js
@@ -1,4 +1,29 @@
 // Create class below
+class Food {
+    constructor (name, daysToSpoil) {
+        this.name = name 
+        this.daysToSpoil = daysToSpoil 
+        this.fresh = true 
+    }
 
+    prepare () {
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh () {
+        console.log(
+            this.daysToSpoil > 0 ?
+            `There are ${this.daysToSpoil} days left before ${this.name} spoils.` :
+            `${this.name} has spoiled.`
+        )
+        this.fresh = this.daysToSpoil === 0 ? false : true
+    }
+
+    aDayPasses () {
+        this.daysToSpoil--
+        this.isFresh()
+    }
+
+}
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,99 @@
 // Create class below
+class Tamagotchi {
+    constructor(name){
+        this.name = name
+        this.energy = 9
+        this.full = 8
+        this.mood = 6
+        this.sick = false
+        this.rehomed = false
+    }
+
+    greet(){
+        console.log(`Hello, I'm ${this.name}!`)
+    }
+
+    status(){
+        console.log(`********************
+My mood is: ${this.mood}
+I am this full: ${this.full}
+My energy is: ${this.energy}
+I am ${this.sick ? '' : `not`} sick
+********************`)
+    }
+
+    eat(){
+        this.full = (this.full + 2)
+        if (this.full > 10){
+            this.sick = true
+        }
+        this.energy--
+    }
+
+    medicate(){
+        if (this.sick){
+            this.sick = false
+            this.full = 9
+            this.energy = (this.energy - 3)
+        } else {
+            this.energy--
+        }
+    }
+
+    play(){
+
+        if (this.sick){
+            this.mood--
+            this.energy--
+        }
+        else if (this.mood > 9){
+            this.energy = (this.energy - 2)
+            this.full --
+        }
+        else if(this.energy <= 3){
+            this.energy-- 
+        }
+        else{
+            this.mood = (this.mood + 2)
+            this.energy--
+            this.full--
+        }
+    }
+
+    sleep(){
+        this.energy = (this.energy + 4)
+        this.full = (this.full - 3)
+    }
+
+    timePasses(){
+        if (this.sick === false){
+            this.mood = (this.mood - 2)
+            this.full--
+            this.energy--
+        }
+        else{
+            this.mood = (this.mood -3)
+            this.full = (this.full - 2)
+            this.energy = (this.energy - 2)
+        }
+    }
+
+    badGuardian(){
+        console.log(`tamagotchi has been rehomed`)
+        if (this.energy <= 0){
+            this.rehomed = true
+        }
+        if (this.mood <= 0){
+            this.rehomed = true
+        }
+        if (this.full <= true){
+            this.rehomed = true
+        }
+    }
+
+
+
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
